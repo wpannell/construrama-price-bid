1f40a10e815a1325eb869f55638dd22a580d9bfa
sfgarage <sfgarage@sfgarages-iMac-5.local>
2016-03-10 16:34:24 -0800

    remove firefox from browser list

--- karma.conf.dev.js
+++ karma.conf.dev.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
     singleRun: true
   });
 };
